c8abe7c752ce2a1bf1ecb722aeb78ca141872944
田中大貴 <138356981+daiki-coprime-tanaka@users.noreply.github.com>
2026-04-06 22:28:38 +0900

    Add files via upload

---
+++ MadeCalc/Tech/TechTextBox/TechTextBox.js
@@ -0,0 +1,6 @@
+function exe() {
+    // alert("Hello.");
+    let x = document.getElementById("text1").value;
+    document.getElementById("x").innerHTML = x;
+    document.getElementById("text1").value = "";
+}
