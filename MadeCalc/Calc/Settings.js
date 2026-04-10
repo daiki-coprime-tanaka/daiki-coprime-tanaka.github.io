adf73ee5daefa91493099a817483ec57e06175b8
田中大貴 <138356981+daiki-coprime-tanaka@users.noreply.github.com>
2026-04-10 16:31:05 +0900

    Update Settings.js

--- MadeCalc/Calc/Settings.js
+++ MadeCalc/Calc/Settings.js
@@ -1,13 +1,13 @@
 function Start() {
     const qty = document.getElementById("qty").value;
     const dif = document.getElementById("dif").value;
-    if(qty <= 0) {
+    if(Number(qty) <= 0) {
         alert("問題数は1以上の整数を入力してください");
         sessionStorage.setItem('qty', "10");
     } else {
         sessionStorage.setItem('qty', qty);
     }
-    if(dif <= 0) {
+    if(Number(dif) <= 0) {
         alert("難易度は1以上の整数を入力してください");
         sessionStorage.setItem('dif', "10");
     } else {
